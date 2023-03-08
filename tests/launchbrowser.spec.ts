3a9eac252155066684cd0ad4c83a0ced366ae0ca
Binay32 <sahbinay432@gmail.com>
2023-03-08 20:52:25 +0545

    launching broswer file

---
+++ tests/launchbrowser.spec.ts
@@ -0,0 +1,11 @@
+import { chromium } from "@playwright/test";
+import { test, expect } from '@playwright/test';
+
+test('visiting google page',async()=>{
+
+    const browser=await chromium.launch();
+    const context= await browser.newContext();
+    const page= await context.newPage();
+    page.goto("https://www.google.com/");
+    await browser.close();
+    })
